3db8c2789973dc4633d6fce640b6e2924c88beb3
yang <903435532@qq.com>
2020-06-14 13:27:33 +0800

    add

--- src/Kinematics.cpp
+++ src/Kinematics.cpp
@@ -1,12 +1,20 @@
 #include "Kinematics.h"
+#include <iostream>
 
 //以机器人正前方为x轴正方向  竖直向上为y轴正方向
 int ang2pos(double *x, double *y , double thet1, double thet2){
     *x = L1*cos(thet1)+L2*cos(thet1+thet2);
     *y = L1*sin(thet1)+L2*sin(thet1+thet2);
+    return 1;
 }
 int pos2ang(double x, double y , double *thet1, double *thet2){
-    double thetp = atan2(y,x);
-    *thet1 = acos((x*x+y*y+L1*L1-L2*L2)*cos(thetp)/(2*L1*x))+thetp;
-    *thet2 = acos((x*x+y*y-L1*L1+L2*L2)*cos(thetp)/(2*L2*x))+thetp-*thet1;
+    double c2,s2;
+    c2 = (x*x+y*y-L1*L1-L2*L2)/(2*L1*L2);
+    if(c2 >= -1 && c2 <= 1) {
+        s2 = -1 * sqrt(1 - c2 * c2);
+        *thet2 = atan2(s2, c2);
+        *thet1 = atan2(y, x) - atan2(L2 * s2, L1 + L2 * c2);
+        return 1;
+    } else
+        return -1;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include "MotorDriver.h"
 #include "Kinematics.h"
+#include <unistd.h>
 #include <iostream>
 
 
 uchar id[4] = {11, 12, 13, 14};
+int max_speed[4] = {};
+int max_acc[4] = {};
+int home_pos[4] = {0};
+int ratio[4] = {41};
+
 int Baudrate = 1000000;
 int pos[4];
 double target_x,target_y;
@@ -12,10 +18,7 @@ double target_thet1,target_thet2;
 MotorDriver motor[4];
 
 int motor_init(){
-    int max_speed[4] = {};
-    int max_acc[4] = {};
-    int home_pos[4] = {0};
-    int ratio[4] = {41};
+
 
     for(int i=1;i<=4;i++){
         motor[i].setMaxSpeed(id[i],max_speed[i]);
@@ -46,13 +49,25 @@ int connect_check()
     return 1;
 
 }
+int cartesian_pos(double target_x, double target_y){
+    double thet1,thet2;
+    pos2ang(target_x,target_y, &thet1,&thet2);
+    motor[0].pos_control(id[0],thet1,ratio[0]);
+    motor[1].pos_control(id[1],thet2,ratio[1]);
+}
+
+
 int main(int argumentCount, const char* argumentValues[])
 {
-    if(connect_check())
+    if(connect_check()) {
         motor_init();
-    ang2pos(&target_x,&target_y,3.14/3,-3.14*2/3);
-    std::cout << target_x << "   " << target_y <<std::endl;
-    pos2ang(target_x,target_y, &target_thet1,&target_thet2);
-    std::cout << target_thet1 << "   " << target_thet2 <<std::endl;
+        sleep(1);
+        cartesian_pos(1,0);
+    }
+    //二连杆正逆运动学测试
+//    ang2pos(&target_x,&target_y,3.14/3,0);
+//    std::cout << target_x << "   " << target_y <<std::endl;
+//    pos2ang(1,-0.5, &target_thet1,&target_thet2);
+//    std::cout << target_thet1 << "   " << target_thet2 <<std::endl;
     return 0;
 }
