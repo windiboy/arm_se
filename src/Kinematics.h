e32ac42a83c1b7b23f67139f4cfa409a9aeaf7a5
yang <903435532@qq.com>
2020-06-15 15:31:14 +0800

    add

--- src/Kinematics.h
+++ src/Kinematics.h
@@ -3,8 +3,8 @@
 
 #include "math.h"
 
-double L1 = 1;
-double L2 = 1;
+double L1 = 0.15;
+double L2 = 0.2;
 int ang2pos(double *x, double *y , double thet1, double thet2);
 int pos2ang(double x, double y , double *thet1, double *thet2);
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,40 +3,63 @@
 #include <unistd.h>
 #include <iostream>
 
+#define PI 3.1415926
+
 
 uchar id[4] = {11, 12, 13, 14};
 int max_speed[4] = {1000};
 int max_acc[4] = {1000};
-int home_pos[4] = {0};
-int ratio[4] = {40};
+int home_pos[4] = {0,0,0,0};
+int ratio[4] = {40,40,40,40};
 
 int Baudrate = 1000000;
-int current_pos[4];
+double current_pos[4];
 double target_x,target_y;
 double target_thet1,target_thet2;
 
 MotorDriver motor;
 
 int read_pos();
+int write_pos(double *target);
 int motor_init(){
-    for(int i=1;i<=4;i++){
-        motor.setMaxSpeed(id[i],max_speed[i]);
-        motor.setAcc(id[i],max_acc[i]);
+    for(int i=0;i<=3;i++){
+//        motor.setMaxSpeed(id[i],max_speed[i]);
+//        motor.setAcc(id[i],max_acc[i]);
         motor.enable(id[i]);
+        motor.mSleep(10);
     }
-    for(int i=1;i<=4;i++) motor.setPos(id[i],home_pos[i]);
+    for(int i=0;i<=3;i++) {
+        motor.setPos(id[i],home_pos[i]);
+        motor.mSleep(10);
+    }
     read_pos();
 }
 int read_pos(){
-    for(int i=1;i<=4;i++)
+    int pos[4];
+    for(int i=0;i<=3;i++)
     {
-        if(!motor.getPos(id[i], &current_pos[i])){
+        if(!motor.getPos(id[i], &pos[i])){
             std::cout << id[i] <<"motor: getpos failed!!!" <<std::endl;
             return -1;
         }
+        motor.mSleep(10);
+        current_pos[i] = pos[i]*PI*2/(262144*ratio[i]);
         std::cout << id[i] <<"motor pos: " << current_pos[i] << std::endl;
     }
 
+}
+int write_pos(double *target){
+    int pos[4];
+    for(int i=0;i<=3;i++) {
+
+        pos[i] = target[i]*262144*ratio[i]/(PI*2);
+
+        pos[0] = -pos[0];
+
+        std::cout << id[i] <<"target pos: " << pos[i] << std::endl;
+        motor.setPos(id[i], pos[i]);
+        motor.mSleep(10);
+    }
 }
 int connect_check()
 {
@@ -51,10 +74,13 @@ int connect_check()
 }
 
 int cartesian_pos(double target_x, double target_y){
-    double thet1,thet2;
-    pos2ang(target_x,target_y, &thet1,&thet2);
-    motor.pos_control(id[0],thet1,ratio[0]);
-    motor.pos_control(id[1],thet2,ratio[1]);
+    double target[4];
+    pos2ang(target_x,target_y, &target[0],&target[1]);
+    target[2] = 0;
+    target[3] = 0;
+
+    std::cout << target[0] << "  " << target[1] << std::endl;
+    write_pos(target);
 }
 
 
@@ -63,7 +89,7 @@ int main(int argumentCount, const char* argumentValues[])
     if(connect_check()) {
         //motor_init();
         sleep(1);
-        //cartesian_pos(1,0);
+        //cartesian_pos(0.25,0);
     }
     //二连杆正逆运动学测试
 //    ang2pos(&target_x,&target_y,3.14/3,0);
