45fc7f9bfcad1724b4a55d3284f01addbe6c5abb
yang <903435532@qq.com>
2020-07-06 17:26:26 +0800

    add

---
+++ src/control_center.cpp
@@ -0,0 +1,33 @@
+#include "ros/ros.h"
+#include "arm_se/ArmControl.h"
+#include "geometry_msgs/Point.h"
+#include <iostream>
+
+using namespace std;
+
+
+void cameraCallback(const geometry_msgs::Point::ConstPtr& msg){
+    &msg = cam_recognition;
+//    ROS_INFO("hello");
+
+}
+int main(int argc, char** argv){
+    ros::init(argc, argv, "control_center");
+    ros::NodeHandle n;
+
+    ros::Publisher arm_pub = n.advertise<arm_se::ArmControl>("arm_control", 50);
+    ros::Subscriber cam_sub = n.subscribe("camera_point",1000,cameraCallback);
+    ros::Rate loop_rate(50);
+
+    arm_se::ArmControl arm_msg;
+    geometry_msgs::Point cam_recognition;
+
+    while (ros::ok()){
+        cout<<"Object position In Robot Coordinate "<<"( "<<cam_recognition.x <<","<<cam_recognition.y<","<< cam_recognition.z <<" )"<<endl;
+
+        loop_rate.sleep();
+        ros::spinOnce();
+    }
+    return 0;
+}
+
