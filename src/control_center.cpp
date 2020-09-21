7dd281c48cb959fe9b36211864542716b0b89bee
yang <903435532@qq.com>
2020-09-21 11:36:14 +0800

    add

--- src/control_center.cpp
+++ src/control_center.cpp
@@ -3,21 +3,16 @@
 #include "geometry_msgs/Point.h"
 #include "geometry_msgs/Twist.h"
 #include "nav_msgs/Odometry.h"
+#include "std_msgs/String.h"
 #include "tf/transform_datatypes.h"
 #include "../include/ArmDriver.h"
 #include <iostream>
+#include <string>
 #include "math.h"
 #include <unistd.h>
 #include <deque>
 
 using namespace std;
-
-arm_se::ArmControl arm_msg;
-geometry_msgs::Twist zoo_msg;
-double object_pos[3];
-double zoo_pos[3];//x y yaw
-MovingAverage x_win,y_win,z_win;
-std_msgs::String audio_cmd;
 
 class MovingAverage {
 public:
@@ -53,124 +48,131 @@ public:
         return (sum*1.0) / mydeque.size();
     }
 };
-
-void cameraCallback(const geometry_msgs::Point::ConstPtr& msg){
-    object_pos[0] = x_win.next(msg->x)-0.15;
-    object_pos[1] = y_win.next(msg->y);
-    object_pos[2] = z_win.next(msg->z)-0.076;
-//    ROS_INFO("hello");
-
-}
-void audioCallback(const std_msgs::String::ConstPtr& msg){
-    audio_cmd = msg->data;
-    ROS_INFO(audio_cmd);
-}
-void odomCallback(const nav_msgs::Odometry::ConstPtr& msg){
-    zoo_pos[0] = msg->pose.pose.position.x;
-    zoo_pos[1] = msg->pose.pose.position.y;
-    tf::Quaternion quat;
-    tf::quaternionMsgToTF(msg->pose.pose.orientation, quat);
-
-    
-    double roll, pitch, yaw;//定义存储r\p\y的容器
-    tf::Matrix3x3(quat).getRPY(roll, pitch, yaw);//进行转换
-    zoo_pos[2] = yaw;
-
-}
-bool zooMove();
-void tryPick(const ros::Subscriber arm_pub, const ros::Publisher zoo_pub){
-    if(zooMove()){
-        zoo_pub.publish(zoo_msg);
-        cout<<"Zoo Moving !!!!!!! "<<endl;
-    } else{
-        cout<<"####### Try Pick ####### "<<endl;
-        double offset = -0.;
-        arm_msg.target_x = 0;
-        arm_msg.target_y = 0;
-        arm_msg.rotation = 0;
-        arm_msg.gripper = 1;
-        arm_msg.platform = object_pos[1]+offset;
-        arm_pub.publish(arm_msg);
-        sleep(5);
-
-        arm_msg.target_x = object_pos[0];
-        arm_msg.target_y = 0;
-        arm_msg.rotation = 0;
-        arm_msg.gripper = 1;
-        arm_msg.platform = object_pos[1]+offset;
-        arm_pub.publish(arm_msg);
-        sleep(3);
-
-        arm_msg.target_x = object_pos[0];
-        arm_msg.target_y = 0;
-        arm_msg.rotation = 0;
-        arm_msg.gripper = 0.3;
-        arm_msg.platform = object_pos[1]+offset;
-        arm_pub.publish(arm_msg);
-        sleep(3);
-        arm_msg.target_x = object_pos[0];
-        arm_msg.target_y = 0;
-        arm_msg.rotation = 0;
-        arm_msg.gripper = 0.3;
-        arm_msg.platform = 0.3;
-        arm_pub.publish(arm_msg);
-        sleep(3);
+class ControlCenter{
+public:
+    ControlCenter(){
+        x_win.init(10);
+        y_win.init(10);
+        z_win.init(10);
     }
-}
+    void audioCallback(const std_msgs::String::ConstPtr& msg){
+        audio_cmd = msg->data;
+    }
+    void odomCallback(const nav_msgs::Odometry::ConstPtr& msg){
+        zoo_pos[0] = msg->pose.pose.position.x;
+        zoo_pos[1] = msg->pose.pose.position.y;
+        tf::Quaternion quat;
+        tf::quaternionMsgToTF(msg->pose.pose.orientation, quat);
+        double roll, pitch, yaw;//定义存储r\p\y的容器
+        tf::Matrix3x3(quat).getRPY(roll, pitch, yaw);//进行转换
+        zoo_pos[2] = yaw;
+    }
+    void cameraCallback(const geometry_msgs::Point::ConstPtr& msg){
+        object_pos[0] = x_win.next(msg->x)-0.15;
+        object_pos[1] = y_win.next(msg->y);
+        object_pos[2] = z_win.next(msg->z)-0.076;
+    }
+    bool zooMove()
+    {
+        double front = 0.55;
+        double rear = 0.45;
+
+        if(object_pos[0]>front)
+            zoo_msg.linear.x = 0.01;
+        if(object_pos[0]<rear)
+            zoo_msg.linear.x = -0.01;
+        if(object_pos[2]>0.01)
+            zoo_msg.linear.y = -0.01;
+        if(object_pos[2]<-0.01)
+            zoo_msg.linear.y = 0.01;
+        if(object_pos[2]>-0.01 && object_pos[2]<0.01)
+        {
+            zoo_msg.linear.y = 0.0;
+        }
+        if(object_pos[0]>rear && object_pos[0]<front)
+        {
+            zoo_msg.linear.x = 0.0;
+        }
+        if(object_pos[2]>-0.005 && object_pos[2]<0.005 && object_pos[0]>rear && object_pos[0]<front)
+        {
+            zoo_msg.linear.y = 0.0;
+            zoo_msg.linear.x = 0.0;
+            return false;
+        }
+        // cout<<zoo_msg<<endl;
+        return true;
+    }
+
+    void tryPick(const ros::Publisher arm_pub, const ros::Publisher zoo_pub){
+        if(zooMove()){
+            zoo_pub.publish(zoo_msg);
+            cout<<"Zoo Moving !!!!!!! "<<endl;
+        } else{
+            cout<<"####### Try Pick ####### "<<endl;
+            double offset = -0.;
+            arm_msg.target_x = 0;
+            arm_msg.target_y = 0;
+            arm_msg.rotation = 0;
+            arm_msg.gripper = 1;
+            arm_msg.platform = object_pos[1]+offset;
+            arm_pub.publish(arm_msg);
+            sleep(5);
+
+            arm_msg.target_x = object_pos[0];
+            arm_msg.target_y = 0;
+            arm_msg.rotation = 0;
+            arm_msg.gripper = 1;
+            arm_msg.platform = object_pos[1]+offset;
+            arm_pub.publish(arm_msg);
+            sleep(3);
+
+            arm_msg.target_x = object_pos[0];
+            arm_msg.target_y = 0;
+            arm_msg.rotation = 0;
+            arm_msg.gripper = 0.3;
+            arm_msg.platform = object_pos[1]+offset;
+            arm_pub.publish(arm_msg);
+            sleep(3);
+            arm_msg.target_x = object_pos[0];
+            arm_msg.target_y = 0;
+            arm_msg.rotation = 0;
+            arm_msg.gripper = 0.3;
+            arm_msg.platform = 0.3;
+            arm_pub.publish(arm_msg);
+            sleep(3);
+        }
+    }
+    double object_pos[3];
+    double zoo_pos[3];//x y yaw
+    string audio_cmd;
+private:
+    arm_se::ArmControl arm_msg;
+    geometry_msgs::Twist zoo_msg;
+    MovingAverage x_win,y_win,z_win;
+};
+
+
 int main(int argc, char** argv){
     ros::init(argc, argv, "control_center");
     ros::NodeHandle n;
 
+    ControlCenter center;
     ros::Publisher arm_pub = n.advertise<arm_se::ArmControl>("arm_control", 50);
     ros::Publisher zoo_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 50);
-    ros::Subscriber cam_sub = n.subscribe("camera_point",1000,cameraCallback);
-    ros::Subscriber odom_sub = n.subscribe("odom",1000,odomCallback);
-    ros::Subscriber audio_sub = n.subscribe("audio",1000,audioCallback);
+    ros::Subscriber cam_sub = n.subscribe("camera_point",1000,&ControlCenter::cameraCallback,&center);
+    ros::Subscriber odom_sub = n.subscribe("odom",1000,&ControlCenter::odomCallback,&center);
+    ros::Subscriber audio_sub = n.subscribe("audio",1000,&ControlCenter::audioCallback,&center);
     ros::Rate loop_rate(50);
-
-    x_win.init(10);
-    y_win.init(10);
-    z_win.init(10);
 
     while (ros::ok()){
         ros::spinOnce();
-
-        cout<<"Object position In Robot Coordinate "<<"( "<<object_pos[0] <<"," << object_pos[1] <<"," << object_pos[2] <<" )"<<endl;
-        cout<<"Zoo position"<<"( "<<zoo_pos[0] <<"," << zoo_pos[1] <<"," << zoo_pos[2] <<" )"<<endl;
+        cout<<"Object position In Robot Coordinate "<<"( "<<center.object_pos[0] <<"," << center.object_pos[1] <<"," << center.object_pos[2] <<" )"<<endl;
+        cout<<"Zoo position"<<"( "<<center.zoo_pos[0] <<"," << center.zoo_pos[1] <<"," << center.zoo_pos[2] <<" )"<<endl;
         cout<<"Arm position"<<"( "<<current_pos[0] <<"," << current_pos[1] <<" )"<<endl;
-        cout<<"Audio commasdfsdnd:"<< audio_cmd << endl;
+        cout<<"Audio command:"<< center.audio_cmd << endl;
+
 
         loop_rate.sleep();
     }
     return 0;
-}
-bool zooMove()
-{
-    double front = 0.55;
-    double rear = 0.45;
-    
-    if(object_pos[0]>front)
-        zoo_msg.linear.x = 0.01;
-    if(object_pos[0]<rear)
-        zoo_msg.linear.x = -0.01;
-    if(object_pos[2]>0.01)
-        zoo_msg.linear.y = -0.01;
-    if(object_pos[2]<-0.01)
-        zoo_msg.linear.y = 0.01;
-    if(object_pos[2]>-0.01 && object_pos[2]<0.01)
-    {
-        zoo_msg.linear.y = 0.0;
-    }
-    if(object_pos[0]>rear && object_pos[0]<front)
-    {
-        zoo_msg.linear.x = 0.0;
-    }
-    if(object_pos[2]>-0.005 && object_pos[2]<0.005 && object_pos[0]>rear && object_pos[0]<front)
-    {
-        zoo_msg.linear.y = 0.0;
-        zoo_msg.linear.x = 0.0;
-        return false;
-    }
-    // cout<<zoo_msg<<endl;
-    return true;
 }
