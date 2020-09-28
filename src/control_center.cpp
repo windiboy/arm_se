3985dda51aacbcbc14aec440b4b8e16d15062fbb
yang <903435532@qq.com>
2020-09-28 14:59:07 +0800

    add

--- src/control_center.cpp
+++ src/control_center.cpp
@@ -106,6 +106,9 @@ public:
     bool zooRotate(const ros::Publisher &zoo_pub, double angle){
         double temp = zoo_pos[2];
         while (zoo_pos[2] < temp + angle){
+            cout<<"Zoo Moving !!!!!!! "<<endl;
+            cout << "zoo angle" << zoo_pos[2] << endl;
+            cout << "target" << temp+angle << endl;
             zoo_msg.angular.z = 1.0;
             zoo_pub.publish(zoo_msg);
         }
